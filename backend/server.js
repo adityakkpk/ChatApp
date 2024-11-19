3127849c22a50f35fa42ed755160af224308c622
Aditya Kumar Kushwaha <akkpk933@gmail.com>
2024-11-19 13:49:01 +0530

    Project Setup

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    // root route http://localhost:3000
+    res.send('Hello, World!');
+});
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
