31d3a4de277b4e30d3d8fc99e6f78043aa0bf393
Aditya Kumar Kushwaha <akkpk933@gmail.com>
2024-12-16 18:38:06 +0530

    Search Functionality

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,15 +1,47 @@
-import React from 'react'
+import React, { useState } from "react";
 import { RiSearch2Line } from "react-icons/ri";
+import useConversation from "../../zustand/useConversation";
+import useGetConversations from "../../hooks/useGetConversation";
+import toast from "react-hot-toast";
 
 const SearchInput = () => {
-  return (
-    <form className='flex items-center gap-2'>
-        <input type="text" placeholder='search' className='input input-bordered rounded-full' />
-        <button type='submit' className='btn btn-circle bg-sky-500 text-white'>
-            <RiSearch2Line className='w-6 h-6 outline-none' />
-        </button>
-    </form>
-  )
-}
+  const [search, setSearch] = useState("");
+  const { setSelectedConversation } = useConversation();
+  const { conversations } = useGetConversations();
 
-export default SearchInput
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!search) return;
+    if (search.length < 3)
+      return toast.error("Search must be at least 3 characters long");
+
+    const conversation = conversations.find((c) =>
+      c.fullName.toLowerCase().includes(search.toLowerCase())
+    );
+
+    if (conversation) {
+      setSelectedConversation(conversation);
+      setSearch("");
+    } else {
+      toast.error("No conversation found with that name");
+      setSearch("");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex items-center gap-2">
+      <input
+        type="text"
+        placeholder="search"
+        className="input input-bordered rounded-full"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button type="submit" className="btn btn-circle bg-sky-500 text-white">
+        <RiSearch2Line className="w-6 h-6 outline-none" />
+      </button>
+    </form>
+  );
+};
+
+export default SearchInput;
