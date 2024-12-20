6dd0066f8f5c7e6c4b53103523521f1ac61c1192
Aditya Kumar Kushwaha <akkpk933@gmail.com>
2024-12-20 20:34:56 +0530

    Fix import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext.jsx";
-import { SocketContextProvider } from "./context/socketContex.jsx";
+import { SocketContextProvider } from "./context/SocketContex.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
